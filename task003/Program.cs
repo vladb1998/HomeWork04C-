c611d113b3dae60d2b7bc92b3060a428c5abe47c
vlad <vladbelyvskij@gmail.com>
2022-08-15 00:26:33 +0300

    Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.

---
+++ task003/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] GetBinaryArray(int size)
+{
+    int[] result = new int [size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next();
+    }
+    return result;
+}
+
+int[] myArray = GetBinaryArray(8);
+Console.WriteLine($"Результат : [{String.Join(";", myArray)}]");
